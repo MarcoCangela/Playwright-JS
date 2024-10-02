b6dc23a1bfc4824df21dd5150fe093681c3f4a7d
Marco Cangela Garujo <Mcangelaoo@gmail.com>
2024-10-02 23:14:10 +0200

    Adding tests with and without browser context and turning headless off

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,11 +1,21 @@
 // Add test anotation for playwright 
-const {test} = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
-test('basic test', async (browser) => {
-//writing tests for chrome using chromium 
-    const page = await browser.newPage();
-    await page.goto('https://www.google.com/');
-    expect(await page.title()).toBe('Google');
-    await page.close();
+    test('basic test with browser', async ({browser}) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto("https://google.com/");
 
-})
+        await page.close();
+        await browser.close();
+    });
+
+    test('basic page test', async ({page}) => {
+        await page.goto("https://google.com");
+        console.log("page title is "+await page.title());
+        console.log("page url is "+await page.url());
+        await expect(page).toHaveTitle("Google");
+        // console.log("page source is "+await page.content());
+        await page.close();
+
+    });
