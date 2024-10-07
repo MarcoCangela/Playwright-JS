dd3ebec25e20407bbf8660c10604287bc2c264e7
Marco Cangela Garujo <Mcangelaoo@gmail.com>
2024-10-07 21:24:49 +0200

    addinh a new test and cleaning up with different selectors

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,2 +1,32 @@
 const {expect, test} = require('@playwright/test');
 
+test('Cleaning Code', async ({ page }) => {
+    //js file- Login js, DashboardPage
+    const email = "markill123@example.com";
+    const productName = 'IPHONE 13 PRO';
+    const products = page.locator(".card-body");
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.getByPlaceholder("email@example.com").fill(email);
+    await page.getByPlaceholder("enter your passsword").fill("K1$$mm1234");
+    await page.getByRole('button',{name:"Login"}).click();
+    await page.waitForLoadState('networkidle');
+    await page.locator(".card-body b").first().waitFor();
+    
+    await page.locator(".card-body").filter({hasText:"IPHONE 13 PRO"})
+    .getByRole("button",{name:"Add to Cart"}).click();
+  
+    await page.getByRole("listitem").getByRole('button',{name:"Cart"}).click();
+  
+    //await page.pause();
+    await page.locator("div li").first().waitFor();
+    await expect(page.getByText("IPHONE 13 PRO")).toBeVisible();
+  
+    await page.getByRole("button",{name :"Checkout"}).click();
+  
+    await page.getByPlaceholder("Select Country").pressSequentially("Moz");
+  
+    await page.getByRole("button",{name :"Mozambique"}).nth(1).click();
+    await page.getByText("PLACE ORDER").click();
+  
+    await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+ })
