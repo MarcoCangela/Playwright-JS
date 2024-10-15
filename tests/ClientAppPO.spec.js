356ca292296ec9d89c961c2866474a9414fbbc66
Marco Cangela Garujo <Mcangelaoo@gmail.com>
2024-10-15 23:51:39 +0200

    Adding some Page Object Models

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -1,21 +1,28 @@
 const {expect, test} = require('@playwright/test');
-const {LoginPage} = require('./pageobjects/LoginPage');
-const {DashboardPage} = require('./pageobjects/DashBoardPage');
+const {POManager} = require('./pageobjects/POManager');
 
 
 test('Cleaning Code', async ({ page }) => {
-    //js file- Login js, DashboardPage
+    const poManager = new POManager(page);
+   
     const username = "markill123@example.com";
     const password = "K1$$mm1234";
-    const productName = 'IPHONE 13 PRO';
+    const productName = 'Zara Coat 4';
     const products = page.locator(".card-body");
-    
-    const loginPage = new LoginPage(page);
+    const loginPage = poManager.getLoginPage();
     await loginPage.gotoPage();
-    await loginPage.validLogin(username,password);
-    const dashBoardPage = new DashboardPage(page);
+    await loginPage.validLogin(username, password);
+    const dashBoardPage = poManager.getDashboardPage();
     await dashBoardPage.searchProductandAddCart(productName);
-    await dashBoardPage.goToCart();
+    await dashBoardPage.gotoCart();
+
+    // const cartPage = poManager.getCartPage();
+    // await cartPage.VerifyProductIsDisplayed(productName);
+    // await cartPage.Checkout();
+
+    
+
+   
 
 
     // await page.locator("div li").first().waitFor();
--- tests/pageobjects/DashBoardPage.js
+++ tests/pageobjects/DashBoardPage.js
@@ -1,30 +1,31 @@
 class DashboardPage {
-    constructor(page) {
-        this.products = page.locator(".card-body");
-        this.productsText = page.locator(".card-body b");
-        this.cart = page.getByRole("listitem").getByRole('button',{name:"Cart"}).click();
+  constructor(page) {
+    this.page = page;
+    this.products = page.locator(".card-body");
+    this.productsText = page.locator(".card-body b");
+    this.cart = page.locator("[routerlink*='cart']");
+    this.orders = page.locator("button[routerlink*='myorders']");
+  }
 
+  async searchProductandAddCart(productName) {
+    const titles = await this.productsText.allTextContents();
+    console.log(titles);
+    const total = await this.products.count();
+    for (let i = 0; i < total; ++i) {
+      if ((await this.products.nth(i).locator("b").textContent()) === productName) {
+        //add to cart
+        await this.products.nth(i).locator("text= Add To Cart").click();
+        break;
+      }
     }
-   
-    async searchProductandAddCart(productName) {
-        const titles = await this.productsText.allTextContents();
-        console.log(titles);
-        console.log(await this.products.count());
-        const count = await this.products.count();
-        for (let i = 0; i < count; i++) {
-            if (await this.products.nth(i).locator("b").textContent() === productName) {
-                //add to cart
-                await this.products.nth(i).locator("[@id="products"]/div[1]/div[2]/div[3]/div/div/button[2]").click();
-                break;
-            }
-        }
-        return -1;
+  }
 
-    }
+  async navigateToOrders() {
+    await this.orders.click();
+  }
 
-    async goToCart() {
-        await this.cart.click();
-        await this.page.waitForLoadState("networkidle");
-    }
+  async gotoCart() {
+    await this.cart.click();
+  }
 }
-module.exports = {DashboardPage}
+module.exports = { DashboardPage };
